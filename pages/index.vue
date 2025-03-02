11feb02121bab20600583ae96ad5a8de062ff604
Lucas <lucas.herman@epitech.eu>
2025-03-02 11:01:41 +0100

    ➕ Accepting Anilist Username and fetching the media list route

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
+const username = ref('');
+
 async function setup() {
-  console.log('setup');
+  console.log(username.value);
+
+  const response = await $fetch('/.proxy/api/fetch_user', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: username.value
+  });
+
+  console.log(response);
 }
+
 </script>
 
 <template>
-  <div class="start">
+  <div class="start" id="app">
     <h1>DAMQ</h1>
     <button @click="setup">Start</button>
+    <div>
+      <label>
+        Your Anilist username:
+        <input type="text" id="username" v-model="username" />
+      </label>
+    </div>
   </div>
 </template>
 
